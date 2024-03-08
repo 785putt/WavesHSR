51a533f076052ca6be5973681ba5cf1a46a40834
FiresoftPH <most.chuti@gmail.com>
2024-03-09 00:30:58 +0700

    Added some test controls (Non-VR) for the animations of the player character

---
+++ Assets/Scripts/HertaScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HertaScript : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetTrigger("Running");
+            anim.ResetTrigger("Idling");
+            anim.ResetTrigger("StrafeLeft");
+            anim.ResetTrigger("StrafeRight");
+            anim.ResetTrigger("Backwards");
+            anim.ResetTrigger("Jumping");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetTrigger("StrafeLeft");
+            anim.ResetTrigger("Idling");
+            anim.ResetTrigger("Running");
+            anim.ResetTrigger("StrafeRight");
+            anim.ResetTrigger("Backwards");
+            anim.ResetTrigger("Jumping");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetTrigger("StrafeRight");
+            anim.ResetTrigger("Idling");
+            anim.ResetTrigger("Running");
+            anim.ResetTrigger("StrafeLeft");
+            anim.ResetTrigger("Backwards");
+            anim.ResetTrigger("Jumping");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetTrigger("Backwards");
+            anim.ResetTrigger("Idling");
+            anim.ResetTrigger("Running");
+            anim.ResetTrigger("StrafeLeft");
+            anim.ResetTrigger("StrafeRight");
+            anim.ResetTrigger("Jumping");
+        }
+        else
+        {
+            anim.SetTrigger("Idling");
+            anim.ResetTrigger("Running");
+            anim.ResetTrigger("StrafeLeft");
+            anim.ResetTrigger("StrafeRight");
+            anim.ResetTrigger("Backwards");
+            anim.ResetTrigger("Jumping");
+        }
+    }
+}
